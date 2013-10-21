36d5448602278227c0f2a878ffba5682f1067bad
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-21 12:08:28 +0100

    changed views to haml, added items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MusicologyApp::Application.routes.draw do
+  get "items/new"
+  get "items/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
