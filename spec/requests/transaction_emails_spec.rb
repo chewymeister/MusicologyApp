b10f4665dbaac6cf58f3bff911be1271585204b8
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-24 16:42:25 +0100

    Red transactions email spec

---
+++ spec/requests/transaction_emails_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'a transaction confirmation email' do
+	it 'is sent when a transaction has been completed' do
+		expect{ Transaction.create(number_of_items: 3) }.to change { emails.count }.by 1
+	end
+
+	context 'should include' do
+		before { Transaction.create(number_of_items: 3) }
+
+		it 'the subject "order confirmation"' do
+			expect(emails.last.subject).to eq "order confirmation"
+		end
+
+		it 'the number of items ordered' do
+			expect(emails.last.body).to match /3 items/
+		end
+	end
+end
